ab1d2bd0b56dcff50273630e78bdcc0c21bd6ed4
EdsonRego <edsonxrego@gmail.com>
2026-01-31 21:35:15 -0300

    Implemented

--- bin/calculadora.dart
+++ bin/calculadora.dart
@@ -1,5 +1,68 @@
-import 'package:calculadora/calculadora.dart' as calculadora;
+import 'dart:io';
+import 'dart:math';
 
-void main(List<String> arguments) {
-  print('Hello world: ${calculadora.calculate()}!');
+void main() {
+  double numeroUm = 0;
+  double numeroDois = 0;
+  String operacao = "";
+
+  void soma() {
+    print(numeroUm + numeroDois);
+  }
+
+  void subtracao() {
+    print(numeroUm - numeroDois);
+  }
+
+  void divisao() {
+    print(numeroUm / numeroDois);
+  }
+
+  void multiplicacao() {
+    print(numeroUm * numeroDois);
+  }
+
+  void calcular() {
+    switch (operacao) {
+      case "+":
+        soma();
+
+      case "-":
+        subtracao();
+
+      case "/":
+        divisao();
+
+      case "*":
+        multiplicacao();
+        break;
+    }
+  }
+
+  print("Digite o primeiro valor: ");
+  String? entrada = stdin.readLineSync();
+
+  if (entrada != null) {
+    if (entrada != "") {
+      numeroUm = double.parse(entrada);
+    }
+  }
+
+  print("Digite uma operacao: ");
+  entrada = stdin.readLineSync();
+  if (entrada != null) {
+    operacao = entrada;
+  }
+
+  print("Digite o segundo valor: ");
+  entrada = stdin.readLineSync();
+  if (entrada != null) {
+    if (entrada != "") {
+      numeroDois = double.parse(entrada);
+    }
+  }
+
+  print("Resultado: ");
+
+  calcular();
 }
